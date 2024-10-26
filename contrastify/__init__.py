6026016dd0b977a7b1963ea991f02cf8dfc08642
CMG203 <cmg203@203LM-2024-02.lan>
2024-10-26 14:09:43 +0200

    chore: Apply .gitignore to remove unnecessary files

--- contrastify/__init__.py
+++
@@ -1,4 +0,0 @@
-# contrastify/__init__.py
-
-from .contrast import compute_contrast
-from .wcag import check_wcag_norms
--- tests/test_wcag.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from contrastify import check_wcag_norms
-
-def test_wcag_checks():
-    contrast_value = 7.5
-    results = check_wcag_norms(contrast_value)
-    
-    assert results['AA'] == True
-    assert results['AAA'] == True
